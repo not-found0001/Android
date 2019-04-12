d83bd3e6e85cd3a0965de496e3d1951aecd73f53
kaisar <mortujakaisar@gmail.com>
2019-04-12 17:59:17 +0600

    CustomToast Example.

--- app/src/main/java/not/mk/testapp/MainActivity.java
+++ app/src/main/java/not/mk/testapp/MainActivity.java
@@ -2,52 +2,38 @@ package not.mk.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
-    private Button logInButton,logOutButton;
-    private EditText EditText1,EditText2;
+    private Button clickButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.FirstTextID);
-
-        logInButton = (Button) findViewById(R.id.AddButtonID);
-        logOutButton = (Button) findViewById(R.id.SubButtonID);
-
-        EditText1 = (EditText) findViewById(R.id.editText1ID);
-        EditText2 = (EditText) findViewById(R.id.editText2ID);
+        clickButton = (Button) findViewById(R.id.clickButtonID);
 
     }
 
-
-
     public void showMessage (View v){
-        try{
-            String number1 = EditText1.getText().toString();
-            String number2 = EditText2.getText().toString();
+        if(v.getId()==R.id.clickButtonID) {
+            LayoutInflater inflater = getLayoutInflater();
+            View customView = inflater.inflate(R.layout.customtoast_layout, (ViewGroup) findViewById(R.id.customToastID));
 
-            double num1 = Double.parseDouble(number1);
-            double num2 = Double.parseDouble(number2);
+            Toast toast = new Toast(MainActivity.this);
 
-            if (v.getId() == R.id.AddButtonID) {
-                double Sum = num1 + num2;
-                textView.setText("Sum is: " + Sum + "\n");
-            } else if (v.getId() == R.id.SubButtonID) {
-                double Sub = num1 - num2;
-                textView.setText("Sub is: " + Sub + "\n");
-            }
-        }catch(Exception e){
-            Toast.makeText(MainActivity.this,"Please Enter Number.",Toast.LENGTH_SHORT).show();
+            toast.setDuration(Toast.LENGTH_SHORT);
+            toast.setView(customView);
+
+            toast.show();
         }
     }
 }
