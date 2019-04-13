192f325a8311d062d9d193894b29963f8f8f907c
kaisar <mortujakaisar@gmail.com>
2019-04-13 19:04:11 +0600

    CheckBox Example.

--- app/src/main/java/not/mk/testapp/MainActivity.java
+++ app/src/main/java/not/mk/testapp/MainActivity.java
@@ -6,34 +6,49 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import static not.mk.testapp.R.id.milkCheckboxID;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textView;
+    private TextView resultTextView;
     private Button clickButton;
+    private CheckBox milkCheckBox,sugarCheckBox,orangeCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        milkCheckBox = (CheckBox) findViewById(R.id.milkCheckboxID);
+        sugarCheckBox = (CheckBox) findViewById(R.id.sugarCheckboxID);
+        orangeCheckBox = (CheckBox) findViewById(R.id.orangeCheckboxID);
         clickButton = (Button) findViewById(R.id.clickButtonID);
+        resultTextView = (TextView) findViewById(R.id.resultTextID);
 
-    }
+        clickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringBuilder stringBuilder = new StringBuilder();
+                if(milkCheckBox.isChecked()){
+                   String val = milkCheckBox.getText().toString();
+                   stringBuilder.append(val+" is Checked.\n\n");
+                }
+                if(sugarCheckBox.isChecked()){
+                    String val = sugarCheckBox.getText().toString();
+                    stringBuilder.append(val+" is Checked.\n\n");
+                }
+                if(orangeCheckBox.isChecked()){
+                    String val = orangeCheckBox.getText().toString();
+                    stringBuilder.append(val+" is Checked.\n\n");
+                }
 
-    public void showMessage (View v){
-        if(v.getId()==R.id.clickButtonID) {
-            LayoutInflater inflater = getLayoutInflater();
-            View customView = inflater.inflate(R.layout.customtoast_layout, (ViewGroup) findViewById(R.id.customToastID));
+                resultTextView.setText(stringBuilder);
+            }
+        });
 
-            Toast toast = new Toast(MainActivity.this);
-
-            toast.setDuration(Toast.LENGTH_SHORT);
-            toast.setView(customView);
-
-            toast.show();
-        }
     }
 }
